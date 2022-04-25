173375fd82d473a92d62cda8a45a4dcfcb08fe3c
TienThanhVu <tienthanhvu0204@gmail.com>
2022-04-25 21:57:17 +0700

    them file hienthingay

---
+++ HienThiNgay.py
@@ -0,0 +1,19 @@
+a = int (input ("Give me a number 'from 1 to 7': "))
+if a == 1:
+    ngay = "Monday"
+elif a == 2:
+    ngay = "Tuesday"
+elif a == 3:
+    ngay = "Wednesday"
+elif a == 4:
+    ngay = "Thurday"
+elif a == 5:
+    ngay = "Friday"
+elif a == 6:
+    ngay = "Saturday"
+elif a == 7:
+    ngay = "Sunday"
+else:
+    ngay = "Error, out of range!"
+print (ngay)
+
